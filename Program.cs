5475545974628ba80fd0030ee8f2db5321468f66
Tiago Sabath <tiago.sabath@gmail.com>
2023-11-03 11:12:32 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using ExemplosAbasstratasInterfacesPOO.Interfaces;
+using ExemplosAbasstratasInterfacesPOO.Models;
+
+Corrente c = new Corrente();
+c.Creditar(500);
+c.ExibirSaldo();
+
+
+
+ICalculadora calc = new Calculadora();
+Console.WriteLine(calc.Multiplicar(3,9));
+Console.WriteLine(calc.Dividir(3,9));
+Console.WriteLine(calc.Somar(3,9));
+Console.WriteLine(calc.Subtrair(3,9));
